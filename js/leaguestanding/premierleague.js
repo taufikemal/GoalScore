26f93ee3a05ac2b08592625f6cc68eea08706e14
amindraa05 <60906676+amindraa05@users.noreply.github.com>
2021-04-27 06:49:46 +0700

    tablestanding

--- js/leaguestanding/premierleague.js
+++ js/leaguestanding/premierleague.js
@@ -4,4 +4,21 @@ fetch('https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=4328&s=2020-2
       let result = data.table;
       result.sort((a,b)=>a.intRank - b.intRank);
       console.log(result)
+
+      let temp = "";
+      result.forEach(item => {
+          temp += "<tr>"
+          temp += `<td>${item.intRank}</td>`
+          temp += `<td><img src=${item.strTeamBadge}></td>`
+          temp += `<td>${item.strTeam}</td>`
+          temp += `<td>${item.intPlayed}</td>`
+          temp += `<td>${item.intWin}</td>`
+          temp += `<td>${item.intDraw}</td>`
+          temp += `<td>${item.intLoss}</td>`
+          temp += `<td>${item.intPoints}</td>`
+          temp += `<td>${item.strForm}</td></tr>`
+          
+      });
+
+      document.getElementById("fetch").innerHTML = temp;
   });
