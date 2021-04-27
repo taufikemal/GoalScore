5c76b82c4ec4cd4da7e98cdf93fb339d1ab0ea6b
amindraa05 <60906676+amindraa05@users.noreply.github.com>
2021-04-27 07:01:09 +0700

    fetchallleague

--- js/leaguestanding/bundesliga.js
+++ js/leaguestanding/bundesliga.js
@@ -1,8 +1,9 @@
-fetch('https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=4328&s=2020-2021')
+fetch('https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=4331&s=2020-2021')
   .then(response => response.json())
   .then((data)=>{
       let result = data.table;
       result.sort((a,b)=>a.intRank - b.intRank);
+      console.log(result)
 
       let temp = "";
       result.forEach(item => {
