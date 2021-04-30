8a555f774364f00b02f356f71167b088514bffd2
amindraa05 <60906676+amindraa05@users.noreply.github.com>
2021-05-01 06:50:30 +0700

    topscorefetch

---
+++ js/playerstats/laliga.js
@@ -0,0 +1,15 @@
+fetch('https://api.football-data.org/v2/competitions/PD/scorers',{
+    "method" : "GET",
+    "headers" : {
+        "X-Auth-Token" : "e88f743b2b444752a685b7d76e59b39e"
+    }
+})
+.then(response => response.json())
+.then((data)=>{
+    let result = data.scorers
+    result.forEach(element => {
+        console.log(element.player.name)
+        console.log(element.team.name)
+        console.log(element.numberOfGoals)
+    });
+})
