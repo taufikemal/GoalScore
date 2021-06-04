5905b68c9d6d17b4fc6ca89d4c6cf026c9fcc217
amindraa05 <60906676+amindraa05@users.noreply.github.com>
2021-06-04 10:45:31 +0700

    deletenews

--- js/livescores/nextmatches.js
+++ js/livescores/nextmatches.js
@@ -1,3 +1,17 @@
-const headbrazil = ()=>{
-    
-}
+const headbrazil = () => {
+  fetch(
+    "https://api.football-data.org/v2/competitions/BSA/matches?status=SCHEDULED",
+    {
+      method: "GET",
+      headers: {
+        "X-Auth-Token": "196ef39bf2654125a7c9e153067a115d",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.matches);
+    });
+};
+
+headbrazil()
